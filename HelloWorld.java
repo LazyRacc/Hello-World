fa68bf2254ecda6bcb8af7acbbd57ec1e2f92e60
Cristian Casanova <lazyracc@gmail.com>
2021-03-09 00:20:50 -0500

    Revert "Update HelloWorld.java"
    This reverts commit c2482cfc4cce2ec32aa82efb7276495e0e4593db.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,14 +3,6 @@ public class HelloWorld
    public static void main(String[]args)
    {
         System.out.println("Hello World");
+       }
        
-       
-       String name = "Raccoon";
-       
-        System.out.println(name);
-        
-        int age = 19;
-        
-        System.out.println(age);
-}
 }
