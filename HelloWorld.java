c2482cfc4cce2ec32aa82efb7276495e0e4593db
Cristian Casanova <lazyracc@gmail.com>
2021-03-09 00:20:07 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,14 @@ public class HelloWorld
    public static void main(String[]args)
    {
         System.out.println("Hello World");
-       }
        
+       
+       String name = "Raccoon";
+       
+        System.out.println(name);
+        
+        int age = 19;
+        
+        System.out.println(age);
+}
 }
